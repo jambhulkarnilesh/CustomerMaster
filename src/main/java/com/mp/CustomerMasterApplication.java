f1a7b16277a713eb415d317923b953308c7e325c
cALL TO niLESH dADA <jnileshjambhulkar@gmail.com>
2020-09-01 16:35:35 +0530

    First Commit with All files

---
+++ src/main/java/com/mp/CustomerMasterApplication.java
@@ -0,0 +1,13 @@
+package com.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerMasterApplication.class, args);
+	}
+
+}
